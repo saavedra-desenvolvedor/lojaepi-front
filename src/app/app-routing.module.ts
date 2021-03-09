a482c14db8940bc181a624f03d4475b2591469db
saavedra-desenvolvedor <saavedra.desenvolvedor@gmail.com>
2021-03-09 20:54:37 -0300

    update para categoria implantado

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,6 +4,7 @@ import { RouterModule, Routes } from '@angular/router';
 import { CategoriaReadComponent } from './components/views/categoria/categoria-read/categoria-read.component';
 import { HomeComponent } from './components/views/home/home.component';
 import { CategoriaDeleteComponent } from './components/views/categoria/categoria-delete/categoria-delete.component';
+import { CategoriaUpdateComponent } from './components/views/categoria/categoria-update/categoria-update.component';
 
 const routes: Routes = [
   {
@@ -21,6 +22,10 @@ const routes: Routes = [
   {
     path : 'categorias/delete/:id',
     component: CategoriaDeleteComponent
+  },
+  {
+    path : 'categorias/update/:id',
+    component: CategoriaUpdateComponent
   }
 ];
 
